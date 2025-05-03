45edab0061ba6d1b94ac4d1ea1064fc853d704c9
hariisgit <shrihari0307@gmail.com>
2025-05-03 21:53:29 +0530

    first commit

---
+++ terraform/terraform.tf
@@ -0,0 +1,17 @@
+# 
+terraform {
+  #Ref - https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  #Ref - https://developer.hashicorp.com/terraform/language/backend/s3
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+}
---
+++ terraform/vpc.tf
@@ -0,0 +1,21 @@
+# Ref - https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "gitops-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east1a", "us-east1b", "us-east1c"]
+  private_subnets = ["10.14.1.0/24", "10.14.2.0/24", "10.14.3.0/24"]
+  public_subnets  = ["10.14.11.0/24", "10.14.12.0/24", "10.14.13.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
